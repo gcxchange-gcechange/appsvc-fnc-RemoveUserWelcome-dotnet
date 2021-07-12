eeb50c8cd529d6a72bec7dce2ed8669fb7e72705
stephanie4104 <stephanie4104@hotmail.com>
2021-07-12 15:46:03 -0400

    Add project files.

---
+++ RemoveUser.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Graph;
+
+namespace appsvc_fnc_RemoveUserWelcome_dotnet
+{
+    public static class RemoveUser
+    {
+        [FunctionName("RemoveUser")]
+         public static async Task<IActionResult> Run([TimerTrigger("0 0 4 * * *")]TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            IConfiguration config = new ConfigurationBuilder()
+
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+            string welcomeGroup = config["welcomeGroup"];
+            Auth auth = new Auth();
+            var graphAPIAuth = auth.graphAuth(log);
+            var result = await CheckMemberWelcome(graphAPIAuth, welcomeGroup, log);
+
+            if(result)
+            {
+                return new OkObjectResult("Function run without issue");
+            }
+            else
+            {
+               return new BadRequestObjectResult("Something happen, please check the logs");
+            }
+        }
+
+        public static async Task<bool> CheckMemberWelcome(GraphServiceClient graphServiceClient, string WelcomeGroup, ILogger log)
+        {
+            bool result = false;
+            try
+            {
+                var groupMember = await graphServiceClient.Groups[WelcomeGroup]
+                .Members
+                .Request()
+                .GetAsync();
+           
+                foreach (DirectoryObject user in groupMember)
+                {
+                    var userInfo = await graphServiceClient.Users[user.Id]
+                            .Request()
+                            .Select("CreatedDateTime")
+                            .GetAsync();
+                    log.LogInformation($" user {userInfo.CreatedDateTime}");
+  
+                    DateTimeOffset UserDate = (DateTimeOffset)userInfo.CreatedDateTime;
+                    int daysLeft = (DateTime.Today - UserDate).Days;
+
+                    if(daysLeft >= 14)
+                    {
+                        await graphServiceClient.Groups[WelcomeGroup].Members[user.Id].Reference
+                            .Request()
+                            .DeleteAsync();
+                        log.LogInformation("User remove");
+                    }
+                    log.LogInformation($"{daysLeft}");
+                }
+                result = true;
+                log.LogInformation("User invite successfully");
+            }
+            catch (ServiceException ex)
+            {
+                log.LogInformation($"Error Creating User Invite : {ex.Message}");
+                result = false;
+            };
+        return result;
+        }
+    }   
+}
+
